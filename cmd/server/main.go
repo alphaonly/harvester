863257672f019f99f1770558010fe17fb06f2674
alphaonly <43378457+alphaonly@users.noreply.github.com>
2023-01-04 14:14:11 +0300

    второй инкремент

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/alphaonly/harvester/cmd/server/handlers"
+	"github.com/alphaonly/harvester/cmd/server/storage"
+	"log"
+	"net/http"
+)
+
+const (
+	// pollInterval   = 2
+	// reportInterval = 3 //10
+
+	serverHost = "127.0.0.1"
+	serverPort = ":8080"
+)
+
+func main() {
+
+	dataServer := storage.DataServer{}.New()
+
+	h := handlers.Handlers{}
+	h.SetDataServer(dataServer)
+
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/update/", h.HandleMetric)
+
+	log.Fatal(http.ListenAndServe(serverPort, nil))
+	fmt.Println(log.Default())
+}

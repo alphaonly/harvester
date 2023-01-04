2ebc7294bb63fc28126ee21fc89f8cce2154574c
alphaonly <43378457+alphaonly@users.noreply.github.com>
2023-01-04 17:23:11 +0300

    commit#12

--- cmd/server/handlers/handlers_test.go
+++ cmd/server/handlers/handlers_test.go
@@ -46,11 +46,11 @@ func TestHandleMetric(t *testing.T) {
 	//Check Url bad unknown namespace
 	urlStr = urlPrefix + "/updater/main.gauge/Alloc/2.36912E+05"
 	r2 := requestParams{method: http.MethodPost, url: urlStr,
-		want: want{code: http.StatusBadRequest, response: `{"status":"ok"}`, contentType: contentType}}
+		want: want{code: http.StatusNotImplemented, response: `{"status":"ok"}`, contentType: contentType}}
 	//Check Url bad unknown metric
 	urlStr = urlPrefix + "/update/main.gauge/Alerrorloc/2.36912E+05"
 	r3 := requestParams{method: http.MethodPost, url: urlStr,
-		want: want{code: http.StatusBadRequest, response: ``, contentType: contentType}}
+		want: want{code: http.StatusOK, response: ``, contentType: contentType}}
 	//Check Url bad method
 	urlStr = urlPrefix + "/update/main.gauge/Alloc/2.36912E+05"
 	r5 := requestParams{method: http.MethodGet, url: urlStr,

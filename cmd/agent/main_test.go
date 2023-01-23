4d7e7c3f050c4b5a69279a0997d710449c243e35
asus <pavel.a.bogomolov@gmail.com>
2023-01-23 15:23:27 +0300

    поправил тест агента

--- cmd/agent/main_test.go
+++ cmd/agent/main_test.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"context"
-	"github.com/alphaonly/harvester/internal/agent"
-	"github.com/stretchr/testify/assert"
-	"reflect"
 	"testing"
 	"time"
+
+	"github.com/alphaonly/harvester/internal/agent"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestUpdateMemStatsMetrics(t *testing.T) {
+func TestUpdate(t *testing.T) {
 
 	tests := []struct {
 		name  string
@@ -17,22 +17,32 @@ func TestUpdateMemStatsMetrics(t *testing.T) {
 		want  bool
 	}{
 		{
-			name: "test#1 - Positive: are there values?",
-			want: false,
+			name:  "test#1 - Positive: are there values?",
+			value: agent.Metrics{},
+			want:  false,
 		},
 	}
+
+	ac := agent.Configuration{
+		PollInterval:   2,
+		ReportInterval: 3, //10
+		ServerHost:     "127.0.0.1",
+		ServerPort:     "8080",
+		UseJSON:        false,
+	}
+
+	a := agent.NewAgent(&ac)
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(tst *testing.T) {
 			ctx := context.Background()
-			//ctxMetrics, cancel := context.WithCancel(ctx)
 			ctxMetrics, cancel := context.WithTimeout(ctx, time.Second*3)
 			defer cancel()
-			go agent.Agent{}.Update(ctxMetrics, &tt.value)
+			go a.Update(ctxMetrics, &tt.value)
 
 			time.Sleep(time.Second * 4)
 
-			if !assert.Equal(t, tt.want, reflect.ValueOf(tt.value).IsZero()) {
+			if !assert.Equal(t, tt.want, tt.value.PollCount > 0) {
 				t.Error("UpdateMemStatsMetrics is not received form runtime values")
 			}
 		})
